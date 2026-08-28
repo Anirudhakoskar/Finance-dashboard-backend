b131fc9ec9a500b097a64ce9cda6a6d4c8ccb70a
Anirudhakoskar <anirudhakoskar@gmail.com>
2026-08-28 12:16:39 +0530

    PORT declaration

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ import dotenv from "dotenv";
 import app from "./src/app.js";
 
 dotenv.config();
-//PORT >>>
+//PORT >>>>
 const PORT = process.env.PORT || 5000;
 
 mongoose.connect(process.env.MONGO_URI)
